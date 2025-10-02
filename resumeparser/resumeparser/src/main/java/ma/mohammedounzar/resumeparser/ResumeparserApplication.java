d54c7715e8b658fe1fe01463c08897e0ebea7335
mohammedounzar <ounzarmohammed@gmail.com>
2025-10-02 14:07:02 +0200

    first commit

---
+++ resumeparser/resumeparser/src/main/java/ma/mohammedounzar/resumeparser/ResumeparserApplication.java
@@ -0,0 +1,13 @@
+package ma.mohammedounzar.resumeparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeparserApplication.class, args);
+	}
+
+}
